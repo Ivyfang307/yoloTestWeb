cdebf81b4f1f3a7d29d598091fcea89026ea8861
Ivyfang307 <ivyfang@stanlco.com>
2019-05-02 00:48:22 +1000

    UPD:JobList.js code refactored

--- src/shared/components/JobList/JobList.jsx
+++ src/shared/components/JobList/JobList.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import moment from 'moment';
 import CompanyLogo from '../../../assets/images/companyLogo.jpg';
 import MapIcon from '../../../assets/images/mapIcon.png';
@@ -11,105 +11,76 @@ class JobList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            job:props.job
+            job: props.job
         };
     }
 
-    componentDidMount(){
-    }
-
-    componentDidUpdate(prevProps, prevState){
-        if(prevProps){
-            if(this.state.job !== this.props.job){
-                console.log('different job  ');
-                this.setState({job:this.props.job});
+    componentDidUpdate(prevProps,prevState) {
+        if (prevProps) {
+            if (this.state.job !== this.props.job) {
+                this.setState({job: this.props.job});
             }
         }
-
     }
 
-    formatCurrency=(value)=>{
-        var formattedValue=null;
-        if(parseInt(value)>=1000){
-            formattedValue=parseInt(value)/1000+'K';
-        }
-        else{
-            formattedValue=value
+    formatCurrency = (value) => {
+        var formattedValue = null;
+        if (parseInt(value) >= 1000) {
+            formattedValue = parseInt(value) / 1000 + 'K';
+        } else {
+            formattedValue = value
         }
         return formattedValue;
     }
 
     render() {
         return (
-            <div className="BottomLine" style={{display:'flex',flexDirection:'column',height:200,width:'100%',alignItems:'flex-start',justifyContent:'center',borderBottomWidth:1,borderBottomColor:'#d8d8d8'}}>
-                <div style={{display:'flex',flexDirection:'row',justifyContent: 'center',flex:1,width:'100%',
-                    alignItems: 'center'}}>
-                    <p style={{fontSize:15,fontWeight: 'bold',color:'#000'}}>
-                        {this.state.job.job_title}
-                    </p>
-                    <div style={{display:'flex',flex:1}}></div>
-                    <p style={{fontSize:15,fontWeight: 'bold',color:'#000'}}>
-                        {'SGD'+this.formatCurrency(this.state.job.salary_range_from )+' - SGD'+this.formatCurrency(this.state.job.salary_range_to)}
-                    </p>
+            <div className="Job-Wrapper">
+                <div className="JobTitle">
+                    {this.state.job.job_title}
+                    <div className="MaxSpace"></div>
+                    {'SGD' + this.formatCurrency(this.state.job.salary_range_from) + ' - SGD' + this.formatCurrency(this.state.job.salary_range_to)}
                 </div>
 
-                <div style={{display:'flex',justifyContent: 'center',flex:1,
-                    alignItems: 'center',flexDirection:'row'}}>
-                    <img
-                        style={{width:20,height:20,marginRight:10,paddingBottom:5,resizeMode:'contain'}}
-                        src={MapIcon}
+                <div className="JobLocation">
+                    <img className="Icon"
+                         src={MapIcon}
                     />
-                    <p style={{fontSize:12,color:'#000'}}>
-                        {this.state.job.company_location}
-                    </p>
-                    <div style={{width:20}}></div>
-                    <img
-                        style={{width:20,height:20,marginRight:10,paddingBottom:5,resizeMode:'contain'}}
-                        src={WorkIcon}
+                    {this.state.job.company_location}
+                    <div className="Space"></div>
+                    <img className="Icon"
+                         src={WorkIcon}
                     />
-                    <p style={{fontSize:12,color:'#000'}}>
-                        {this.state.job.xp_lvl}
-                    </p>
+                    {this.state.job.xp_lvl}
                 </div>
 
-                <div style={{display:'flex',justifyContent: 'center',flex:1,
-                    alignItems: 'center',flexDirection:'row'}}>
-                    <img
-                        style={{width:20,height:20,marginRight:10,paddingBottom:5,resizeMode:'contain'}}
-                        src={QualificationIcon}
+                <div className="JobLocation">
+                    <img className="Icon"
+                         src={QualificationIcon}
                     />
-                    <p style={{fontSize:12,color:'#000'}}>
-                        {this.state.job.degree}
-                    </p>
-                    <div style={{width:20}}></div>
-                    <img
-                        resizeMode={'contain'}
-                        style={{width:10,height:10,marginRight:10,paddingBottom:5,resizeMode:'contain'}}
-                        src={ClockIcon}
+                    {this.state.job.degree}
+                    <div className="Space"></div>
+                    <img className="ClockIcon"
+                         src={ClockIcon}
                     />
-                    <p style={{fontSize:12,fontWeight: 'bold',color:'#000'}}>
-                        {this.state.job.job_type}
-                    </p>
+                    {this.state.job.job_type}
                 </div>
 
-                <div style={{display:'flex',justifyContent: 'center',flex:1,
-                    alignItems: 'center',flexDirection:'row'}}>
+                <div className="JobLocation">
                     <img
-                        style={{width:40,height:30,marginRight:20,paddingBottom:5,resizeMode:'contain'}}
+                        className="CompanyLogo"
                         src={CompanyLogo}
                     />
-                    <div style={{width:180}}>
-                        <p numberOfLines={2} ellipsizeMode={'tail'} style={{fontSize:12,color:'#000'}}>
+                    <div style={{width: 180}}>
+                        <p numberOfLines={2} ellipsizeMode={'tail'}>
                             {this.state.job.company_name}
                         </p>
                     </div>
-                    <div style={{display:'flex',flex:1}}></div>
-                    <p style={{fontSize:15,color:'grey'}}>
-                        { moment(this.state.job.updated_at).fromNow()}
+                    <div className="MaxSpace"></div>
+                    <p style={{color: 'grey'}}>
+                        {moment(this.state.job.updated_at).fromNow()}
                     </p>
                 </div>
-
-
 
             </div>
         );
